aa7e059922cffeb1fd981b5a9906876b24f3c20e
byGamsa <larsgerigk@gmx.de>
2019-04-27 00:47:01 +0200

    Add Pickup

---
+++ KellnerGame/Assets/Scripts/Objects/Pickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    public Transform Dest;
+    public GameObject Object;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Object.GetComponent<Rigidbody>().useGravity = false;
+            Object.transform.position = Dest.position;
+            Object.transform.parent = GameObject.Find("ItemDestination").transform;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            
+        }
+    }
+}
